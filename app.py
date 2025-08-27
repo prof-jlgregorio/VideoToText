005ad23db310647b407d5d38a02f89de61118d72
Prof. J. L. Gregório <jlgregorio81@hotmail.com>
2025-08-27 19:20:27 -0300

    Add spiner, temp folder and update .gitignore

--- app.py
+++ app.py
@@ -1,20 +1,28 @@
-import moviepy as mp #pip install moviepy
-import speech_recognition as sr #pip install SpeechRecognition
+#python versão 3.13.3
+import moviepy as mp  # pip install moviepy
+import speech_recognition as sr  # pip install SpeechRecognition
+from yaspin import yaspin #spinner para animação de processando
 
-videoFile = "ep02.mp4"
+videoFile = "./temp/video01.mp4" #carrega o arquivo - caminho do arquivo
 
-fileClip = mp.VideoFileClip(videoFile).subclipped
+# Extrair áudio do vídeo
+fileClip = mp.VideoFileClip(videoFile)
+fileClip.audio.write_audiofile("./temp/audio.wav")
 
-fileClip.audio.write_audiofile("audio.wav")
+recognizer = sr.Recognizer() # Criar reconhecedor
 
-
-recognizer = sr.Recognizer()
-
-with sr.AudioFile("audio.wav") as audioSource:
-    print("Gerando transcrição. Aguarde...")
-    audio = recognizer.record(audioSource)
-    text = recognizer.recognize_google(audio, language='pt-BR')
-    textFile = open("transcription.txt", 'w')
-    textFile.write(text)
-    textFile.close
-    print("Transcrição gerada com sucesso!")
+#usando o arquivo de áudio gerado...
+with sr.AudioFile("./temp/audio.wav") as audioSource:
+    #print("Gerando transcrição. Aguarde...")
+    with yaspin(text="Gerando transcrição") as spinner:
+        audio = recognizer.record(audioSource)  # lê o áudio inteiro
+        #usa o reconhecedor de fala para gerar o arquivo texto
+        try:
+            text = recognizer.recognize_google(audio, language='pt-BR')
+            with open("./temp/transcription.txt", "w", encoding="utf-8") as textFile:
+                textFile.write(text)
+            spinner.ok("✅")  # muda para ✔ qdo terminar
+        except sr.UnknownValueError:
+            print("❌ Não foi possível entender o áudio.")
+        except sr.RequestError as e:
+            print(f"❌ Erro ao acessar serviço de reconhecimento: {e}")
